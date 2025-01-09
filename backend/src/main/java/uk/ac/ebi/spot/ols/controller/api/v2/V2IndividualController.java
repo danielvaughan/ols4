17017eeea1b0a3f9f142f0973d38a8ac39121a65
haider <haideriqbal1@hotmail.com>
2025-01-09 13:40:47 +0000

    Add Swagger Documentation for V2 API

--- backend/src/main/java/uk/ac/ebi/spot/ols/controller/api/v2/V2IndividualController.java
+++ backend/src/main/java/uk/ac/ebi/spot/ols/controller/api/v2/V2IndividualController.java
@@ -1,5 +1,7 @@
 package uk.ac.ebi.spot.ols.controller.api.v2;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
@@ -11,10 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriUtils;
 import uk.ac.ebi.spot.ols.controller.api.v2.helpers.DynamicQueryHelper;
 import uk.ac.ebi.spot.ols.controller.api.v2.responses.V2PagedAndFacetedResponse;
@@ -30,7 +29,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@Tag(name = "V2 Individual Controller", description = "This endpoint provides access to individuals information.")
+@RestController
 @RequestMapping("/api/v2")
 public class V2IndividualController {
 
@@ -41,12 +41,34 @@ public class V2IndividualController {
     public HttpEntity<V2PagedAndFacetedResponse<V2Entity>> getIndividuals(
             @PageableDefault(size = 20, page = 0) Pageable pageable,
             @RequestParam(value = "lang", required = false, defaultValue = "en") String lang,
-            @RequestParam(value = "search", required = false) String search,
-            @RequestParam(value = "searchFields", required = false) String searchFields,
-            @RequestParam(value = "boostFields", required = false) String boostFields,
-            @RequestParam(value = "exactMatch", required = false, defaultValue = "false") boolean exactMatch,
-            @RequestParam(value = "includeObsoleteEntities", required = false, defaultValue = "false") boolean includeObsoleteEntities,
-            @RequestParam MultiValueMap<String,String> searchProperties
+            @RequestParam(value = "search", required = false)
+            @Parameter(name="search",
+                    description = "This parameter specify the search query text.",
+                    example = "liver disease") String search,
+            @RequestParam(value = "searchFields", required = false)
+            @Parameter(name = "searchFields",
+                    description = "This parameter is a white space separated list of fields to search in. " +
+                            "The fields are weighted equally. The fields are defined in the schema. " +
+                            "The default fields are label and definition. " +
+                            "The fields weights can be boosted by appending a caret ^ and a positive integer to the field name. " +
+                            "For example, label^3 synonyms^2 description^1 logical_definition^1",
+                    example = "label^100 description") String searchFields,
+            @RequestParam(value = "boostFields", required = false)
+            @Parameter(name = "boostFields",
+                    description = "This parameter is a white space separated list of fields appended with a caret to boost in search. " +
+                            "The default fields are type, is_defining_ontology, label, curie, shortForm and synonym . " +
+                            "The fields weights can be boosted by appending a caret ^ and a positive integer to the field name. ",
+                    example = "label^100 curie^50") String boostFields,
+            @RequestParam(value = "exactMatch", required = false, defaultValue = "false")
+            @Parameter(name = "exactMatch",
+                    description = "As the name suggests its a boolean parameter to specify if search should be exact match or not." +
+                            "The default value is false") boolean exactMatch,
+            @RequestParam(value = "includeObsoleteEntities", required = false, defaultValue = "false")
+            @Parameter(name = "includeObsoleteEntities",
+                    description = "A boolean parameter to specify if obsolete entities should be included or not. Default value is false.") boolean includeObsoleteEntities,
+            @RequestParam
+            @Parameter(name="searchProperties",
+                    description = "Specify any other search field here which are not specified by searchFields or boostFields.") MultiValueMap<String,String> searchProperties
     ) throws ResourceNotFoundException, IOException {
 
         Map<String, Collection<String>> properties = new HashMap<>();
@@ -64,14 +86,39 @@ public class V2IndividualController {
     @RequestMapping(path = "/ontologies/{onto}/individuals", produces = {MediaType.APPLICATION_JSON_VALUE, MediaTypes.HAL_JSON_VALUE}, method = RequestMethod.GET)
     public HttpEntity<V2PagedAndFacetedResponse<V2Entity>> getIndividuals(
             @PageableDefault(size = 20, page = 0) Pageable pageable,
-            @PathVariable("onto") @NotNull String ontologyId,
+            @PathVariable("onto") @NotNull
+            @Parameter(name = "onto",
+                    description = "Ontology Id to search individuals in.",
+                    example = "efo") String ontologyId,
             @RequestParam(value = "lang", required = false, defaultValue = "en") String lang,
-            @RequestParam(value = "search", required = false) String search,
-            @RequestParam(value = "searchFields", required = false) String searchFields,
-            @RequestParam(value = "boostFields", required = false) String boostFields,
-            @RequestParam(value = "exactMatch", required = false, defaultValue = "false") boolean exactMatch,
-            @RequestParam(value = "includeObsoleteEntities", required = false, defaultValue = "false") boolean includeObsoleteEntities,
-            @RequestParam MultiValueMap<String,String> searchProperties
+            @RequestParam(value = "search", required = false)
+            @Parameter(name="search",
+                    description = "This parameter specify the search query text.",
+                    example = "liver disease") String search,
+            @RequestParam(value = "searchFields", required = false)
+            @Parameter(name = "searchFields",
+                    description = "This parameter is a white space separated list of fields to search in. " +
+                            "The fields are weighted equally. The fields are defined in the schema. " +
+                            "The default fields are label and definition. " +
+                            "The fields weights can be boosted by appending a caret ^ and a positive integer to the field name. " +
+                            "For example, label^3 synonyms^2 description^1 logical_definition^1",
+                    example = "label^100 description") String searchFields,
+            @RequestParam(value = "boostFields", required = false)
+            @Parameter(name = "boostFields",
+                    description = "This parameter is a white space separated list of fields appended with a caret to boost in search. " +
+                            "The default fields are type, is_defining_ontology, label, curie, shortForm and synonym . " +
+                            "The fields weights can be boosted by appending a caret ^ and a positive integer to the field name. ",
+                    example = "label^100 curie^50") String boostFields,
+            @RequestParam(value = "exactMatch", required = false, defaultValue = "false")
+            @Parameter(name = "exactMatch",
+                    description = "As the name suggests its a boolean parameter to specify if search should be exact match or not." +
+                            "The default value is false") boolean exactMatch,
+            @RequestParam(value = "includeObsoleteEntities", required = false, defaultValue = "false")
+            @Parameter(name = "includeObsoleteEntities",
+                    description = "A boolean parameter to specify if obsolete entities should be included or not. Default value is false.") boolean includeObsoleteEntities,
+            @RequestParam
+            @Parameter(name="searchProperties",
+                    description = "Specify any other search field here which are not specified by searchFields or boostFields.") MultiValueMap<String,String> searchProperties
     ) throws ResourceNotFoundException, IOException {
 
         Map<String, Collection<String>> properties = new HashMap<>();
@@ -88,8 +135,14 @@ public class V2IndividualController {
 
     @RequestMapping(path = "/ontologies/{onto}/individuals/{individual}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaTypes.HAL_JSON_VALUE}, method = RequestMethod.GET)
     public HttpEntity<V2Entity> getIndividual(
-            @PathVariable("onto") String ontologyId,
-            @PathVariable("individual") String iri,
+            @PathVariable("onto")
+            @Parameter(name = "onto",
+                    description = "Ontology Id to get the information about.",
+                    example = "efo") String ontologyId,
+            @PathVariable("individual")
+            @Parameter(name = "individual",
+                    description = "The IRI of the individual, this value must be double URL encoded",
+                    example = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FIAO_0000002") String iri,
             @RequestParam(value = "lang", required = false, defaultValue = "en") String lang
     ) throws ResourceNotFoundException {
 
@@ -104,8 +157,14 @@ public class V2IndividualController {
     @RequestMapping(path = "/ontologies/{onto}/classes/{class}/individuals", produces = {MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.GET)
     public HttpEntity<V2PagedResponse<V2Entity>> getClassIndividuals(
             @PageableDefault(size = 20, page = 0) Pageable pageable,
-            @PathVariable("onto") String ontologyId,
-            @PathVariable("class") String classIri,
+            @PathVariable("onto")
+            @Parameter(name = "onto",
+                    description = "Ontology Id to get the information about.",
+                    example = "efo") String ontologyId,
+            @PathVariable("class")
+            @Parameter(name = "class",
+                    description = "The IRI of the class, this value must be double URL encoded",
+                    example = "http%3A%2F%2Fwww.ebi.ac.uk%2Fefo%2FEFO_1000967") String classIri,
             @RequestParam(value = "lang", required = false, defaultValue = "en") String lang
     ) throws ResourceNotFoundException, IOException {
 
